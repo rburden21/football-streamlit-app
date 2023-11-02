f0be15ec65b65236bf9f98e4ad1ad3b9b3620e16
Richard Burden <richard@Richards-MacBook-Air.local>
2023-11-02 10:43:25 +0000

    updated with gameweek 10 data

--- football-streamlit-app.py
+++ football-streamlit-app.py
@@ -233,7 +233,7 @@ def page_two():
     fig81 = gp.plot_fixture_difficulty(team_of_interest, window = 4)
     st.pyplot(fig81)
 
-    styled_df = gp.fixture_heatmap(current_gameweek = 10, match_num = 8)
+    styled_df = gp.fixture_heatmap(current_gameweek = 11, match_num = 8)
     st.dataframe(styled_df)
 
     fig6 = gp.age_histogram(df_player, team_of_interest)
