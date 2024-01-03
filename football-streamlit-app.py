35a0b1996e516ce414a0b3d938aee7aff25b2e88
Richard Burden <richard@Richards-MacBook-Air.local>
2024-01-03 10:53:53 +0000

    update weekly data for gameweek 20

--- football-streamlit-app.py
+++ football-streamlit-app.py
@@ -23,7 +23,7 @@ axes_settings = {
 
 plt.rc("axes", **axes_settings)
 
-start_week = 20
+start_week = 21
 
 gen_attributes = ['G-PK','npxG','Sh','Ast','xAG','npxG+xAG', 'Shot Creating Actions','Progressive Passes', 'PrgR']
 
