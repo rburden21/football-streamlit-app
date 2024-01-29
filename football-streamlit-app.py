824511f4714f117699a01e10a6544a31df153a4a
Richard Burden <richard@Richards-MacBook-Air.local>
2024-01-29 10:08:36 +0000

    updating week 21 data

--- football-streamlit-app.py
+++ football-streamlit-app.py
@@ -23,7 +23,7 @@ axes_settings = {
 
 plt.rc("axes", **axes_settings)
 
-start_week = 21
+start_week = 22
 
 gen_attributes = ['G-PK','npxG','Sh','Ast','xAG','npxG+xAG', 'Shot Creating Actions','Progressive Passes', 'PrgR']
 
